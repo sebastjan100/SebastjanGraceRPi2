a0f6e9df0e05fb19b963e2ba780810f2fcc01978
Sebastjan Tekavc <73191480+sebastjan100@users.noreply.github.com>
2022-04-06 15:40:00 +0000

    bug fixing

--- pir.py
+++ pir.py
@@ -7,27 +7,27 @@ GPIO.setmode(GPIO.BCM)
 last_move = None
 
 def initialize(pir):
-        GPIO.setup(pir, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+    GPIO.setup(pir, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 #define functions which will be triggered on pin state changes
 def pirMove(pir):
-        def pirInterupt(channel):
-                global last_move
-                print("Premik")
-                last_move = time.time()
-        return pirInterupt
+    def pirInterupt(channel):
+        global last_move
+        print("Premik")
+        last_move = time.time()
+    return pirInterupt
 
 def getLastMove():
-  global last_move
-        return last_move
+    global last_move
+    return last_move
 
 if __name__ == "__main__":
-        pir = 13
-        initialize(pir)
-        try:
-                GPIO.add_event_detect(pir, GPIO.FALLING, callback=pirMove(pir), bouncetime=100)
-                input("Test pir\n")
-        except KeyboardInterrupt:
-                print("Ctl C pressed - ending program")
+    pir = 13
+    initialize(pir)
+    try:
+        GPIO.add_event_detect(pir, GPIO.FALLING, callback=pirMove(pir), bouncetime=100)
+        input("Test pir\n")
+    except KeyboardInterrupt:
+        print("Ctl C pressed - ending program")
 
-# GPIO.cleanup() # resets GPIO ports used back to input mode
+# GPIO.cleanup() # resets GPIO ports used back to input mode a zdej je DA :D probi bom
